a74e956e215507a7bd1bda4e2049e0563372491d
Student <example@example.com>
2019-08-20 10:16:33 -0700

    add response concern for json response

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,36 @@
 class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+    json_response(@groups)
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    json_response(@group)
+  end
+
+  def create
+    @group = Group.create(group_params)
+    json_response(@group)
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def group_params
+    params.permit(:title)
+  end
 end
