04d4736d7af2e8639428930c557ef9c851b1008c
Student <example@example.com>
2019-08-20 10:24:42 -0700

    add versioning to api

--- app/controllers/v1/groups_controller.rb
+++ app/controllers/v1/groups_controller.rb
@@ -1,4 +1,4 @@
-class GroupsController < ApplicationController
+class V1::GroupsController < ApplicationController
 
   def index
     @groups = Group.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  concern :api_base do
     resources :groups do
       resources :messages
     end
+  end
+
+  namespace :v1 do
+    concerns :api_base
+  end
 end
